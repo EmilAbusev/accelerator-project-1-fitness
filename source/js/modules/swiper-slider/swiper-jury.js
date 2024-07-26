6782f007ea60eac4948d3411b4b50aa5018c5bb8
Emil Abusev <emilabusev.yandex.ru>
2024-07-26 12:48:57 +0300

    Исправляет ошибку в файле swiper-jury.js

--- source/js/modules/swiper-slider/swiper-jury.js
+++ source/js/modules/swiper-slider/swiper-jury.js
@@ -7,31 +7,33 @@ const swiperSlider = document.querySelector('.swiper');
 const buttonNext = document.querySelector('.swiper-button-next');
 const buttonPrev = document.querySelector('.swiper-button-prev');
 
-const mySwiper = new Swiper(swiperSlider, {
-  modules: [Navigation],
+const mySwiper = () => {
+  new Swiper(swiperSlider, {
+    modules: [Navigation],
 
-  navigation: {
-    nextEl: buttonNext,
-    prevEl: buttonPrev,
-  },
+    navigation: {
+      nextEl: buttonNext,
+      prevEl: buttonPrev,
+    },
 
-  loop: true,
+    loop: true,
 
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 20,
-    },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 40,
-    },
-    1366: {
-      slidesPerView: 4,
-      spaceBetween: 40,
-    },
-  }
-});
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1366: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+    }
+  });
+};
 
 const initSwiperTours = () => {
   if (document.body.contains(swiperSlider)) {
