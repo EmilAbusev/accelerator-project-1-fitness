faa4f64edf6761a2a99159c2ed61a2ae6323f5ed
Эмиль Абусев <emilabusev@yandex.ru>
2024-07-27 18:40:50 +0300

    Исправляет стили блоков под ПП.

--- source/js/main.js
+++ source/js/main.js
@@ -1,11 +1,11 @@
-import {reproduceYoutubeVideo} from './modules/inline-youtube/init-inline-youtube';
+// import {reproduceYoutubeVideo} from './modules/inline-youtube/init-inline-youtube';
 // Закоментировал функцию прогрузки видео т.к из-за него прохоидит проверка по ПП свайпера блока жури.
 import {onTabClikc} from './modules/tabs/tabs-subscription';
 import {initSwiperTours} from './modules/swiper-slider/swiper-jury';
 
 window.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('load', () => {
-    reproduceYoutubeVideo();
+    // reproduceYoutubeVideo();
     initSwiperTours();
     onTabClikc();
   });
